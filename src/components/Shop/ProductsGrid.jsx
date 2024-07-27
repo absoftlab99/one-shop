55978e963d8fd70c27ea50533cea9efc77e52497
absoftlab99 <abdullahtech2@outlook.com>
2024-07-27 13:28:38 +0600

    set show hide animation using framer motion

--- src/components/Shop/ProductsGrid.jsx
+++ src/components/Shop/ProductsGrid.jsx
@@ -1,23 +1,37 @@
+'use client'
 import { IconEye, IconHeart, IconHeartFilled, IconShoppingCartPlus, IconStarFilled } from '@tabler/icons-react';
 import React, { useState } from 'react';
 import QuickView from './QuickView';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
+
 const ProductsGrid = ({ product }) => {
     const { id, name, thumbnail, short_description, description, price, sale_price, brand, category, rating, discount, stock_count } = product;
     const [wishlist, setWishlist] = useState(false);
+    const [animate, setAnimate] = useState(false)
     // console.log(name);
+
+    const toggleAnimation = () => {
+        setAnimate(!animate)
+    };
     return (
         <div className='col-span-12 md:col-span-4 lg:col-span-3'>
-            <div className="card card-bordered border-primary">
+            <div onMouseEnter={() => { setAnimate(true); toggleAnimation() }} onMouseLeave={() => setAnimate(false)} className="card card-bordered border-primary">
                 <figure className='relative'>
                     {/* <div className='skeleton h-[250px] w-full rounded-b-none'></div> */}
-                    <Image src={thumbnail} width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} alt='product image'/>
+                    <Image className={animate ? 'scale-110 transition-all duration-500' : 'scale-100 transition-all duration-500'} src={thumbnail} width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} alt='product image' />
                     <div className='absolute top-5 left-0 bg-primary rounded-full rounded-s-none px-2 text-white text-[12px] md:text-[14px]'>{brand}</div>
-                    <div className="flex gap-2 absolute bottom-4 right-4">
-                        <div onClick={() => document.getElementById(`${id}`).showModal()} className="bg-primary rounded-full p-2 cursor-pointer"><IconEye className='text-white' size={20}></IconEye></div>
-                        <div className="bg-primary rounded-full p-2 cursor-pointer"><IconShoppingCartPlus className='text-white' size={20}></IconShoppingCartPlus></div>
-                        <div className="bg-primary rounded-full p-2 cursor-pointer">{wishlist ? <IconHeartFilled onClick={() => setWishlist(false)} className='text-error cursor-pointer' size={20}></IconHeartFilled> : <IconHeart onClick={() => setWishlist(true)} className='text-white cursor-pointer' size={20}></IconHeart>}</div>
-                    </div>
+                    <motion.dev
+                    initial={{ y: 0 }}
+                    animate={{ y: animate ? 170 : 250 }}
+                    transition={{ ease: "easeIn", duration: animate ? 0.3 : 0.3 }}
+                    >
+                        <div className={`flex gap-2 absolute bottom-4 right-4 bg-primary p-2 rounded-xl`}>
+                            <div onClick={() => document.getElementById(`${id}`).showModal()} className="bg-primary rounded-full p-2 cursor-pointer"><IconEye className='text-white' size={20}></IconEye></div>
+                            <div className="bg-accent rounded-full p-2 cursor-pointer"><IconShoppingCartPlus className='text-white' size={20}></IconShoppingCartPlus></div>
+                            <div className="bg-primary rounded-full p-2 cursor-pointer">{wishlist ? <IconHeartFilled onClick={() => setWishlist(false)} className='text-error cursor-pointer' size={20}></IconHeartFilled> : <IconHeart onClick={() => setWishlist(true)} className='text-white cursor-pointer' size={20}></IconHeart>}</div>
+                        </div>
+                    </motion.dev>
                 </figure>
                 <div className="card-body p-4 flex flex-col justify-between min-h-[175px]">
                     <div className="card-title">

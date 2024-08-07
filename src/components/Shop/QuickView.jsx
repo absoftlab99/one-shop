0bb6df29e1ff5b6ae04b68ac61e81ba167396b33
absoftlab99 <abdullahtech2@outlook.com>
2024-08-07 14:56:32 +0600

    qucick view design completed

--- src/components/Shop/QuickView.jsx
+++ src/components/Shop/QuickView.jsx
@@ -1,7 +1,8 @@
+import Image from 'next/image';
 import React from 'react';
 
-const QuickView = ({product}) => {
-    const { id, name, short_description, description, price, sale_price, brand, category, rating, discount, stock_count } = product;
+const QuickView = ({ product }) => {
+    const { id, thumbnail, name, short_description, description, price, sale_price, brand, category, rating, discount, stock_count } = product;
 
     return (
         <div>
@@ -9,8 +10,32 @@ const QuickView = ({product}) => {
             {/* <button className="btn" onClick={() => document.getElementById('my_modal_4').showModal()}>open modal</button> */}
             <dialog id={id} className="modal">
                 <div className="modal-box w-11/12 max-w-5xl">
-                    <h3 className="font-bold text-lg">{name}</h3>
-                    <p className="py-4">Click the button below to close</p>
+                    <div className="grid grid-cols-12 gap-5">
+                        <div className="col-span-12 md:col-span-5">
+                            <Image className="border rounded-xl" src={thumbnail} width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} alt='product image' />
+                        </div>
+                        <div className="col-span-12 md:col-span-7">
+                            <div className="badge badge-warning text-white">{discount}</div>
+                            <h1 className='text-[20px] md:text-[28px]'>{name}</h1>
+                            <div className='flex justify-between items-center'>
+                                <div className="flex gap-3">
+                                    <p className='font-bold text-[24px] text-primary'>{sale_price}</p>
+                                    <s className='font-light text-[24px] text-error'>{price}</s>
+                                </div>
+                                <div className="flex gap-4 items-center">
+                                    <div className="rating rating-sm">
+                                        <input type="radio" name="rating-1" className="mask mask-star bg-yellow-400" />
+                                        <input type="radio" name="rating-1" className="mask mask-star bg-yellow-400" />
+                                        <input type="radio" name="rating-1" className="mask mask-star bg-yellow-400" />
+                                        <input type="radio" name="rating-1" className="mask mask-star bg-yellow-400" defaultChecked />
+                                        <input type="radio" name="rating-1" className="mask mask-star bg-yellow-400" />
+                                    </div>
+                                    <p className='text-[14px]'>38 Customer Reviews</p>
+                                </div>
+                            </div>
+                            <div className="text-[14px] mt-5">{description.slice(0, 280)}...</div>
+                        </div>
+                    </div>
                     <div className="modal-action">
                         <form method="dialog">
                             {/* if there is a button, it will close the modal */}
